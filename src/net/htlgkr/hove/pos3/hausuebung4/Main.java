b0c650af6f665b81c1e90c4d899931a02029f5f6
vhofstaetter <hofstaetterv190219@sus.htl-grieskirchen.at>
2022-10-07 11:00:07 +0200

    CheckIfNumberCorrect

--- src/net/htlgkr/hove/pos3/hausuebung4/Main.java
+++ src/net/htlgkr/hove/pos3/hausuebung4/Main.java
@@ -17,15 +17,13 @@ public class Main {
         List<Integer> numbersList = null;
         try {
             Scanner scanner = new Scanner(new File("numbers.csv"));
-            scanner.nextLine();
             numbersList = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] parts = line.split(":");
                 for (int i = 0; i < parts.length; i++) {
-                    if (parts[i] != null && parts[i].equals("") && parts[i].matches("[+-]?\\d*(\\.\\d+)?")) {
-                        int tmp = Integer.parseInt(parts[i]);
-                        numbersList.add(tmp);
+                    if (parts[i] != null && !parts[i].equals("") && parts[i].matches("[+-]?\\d*(\\.\\d+)?")) {
+                        numbersList.add(Integer.valueOf(parts[i]));
                     }
                 }
             }
